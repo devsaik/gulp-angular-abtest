cd5af59eebde3aae974426e05f97e1119444cfff
devsaik <emailsaik@gmail.com>
2016-01-12 12:12:55 -0800

    heroku ready

---
+++ web.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rca733 on 1/12/16.
+ */
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
