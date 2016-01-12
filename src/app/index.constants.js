0861a89df398c7f6060e3d7114a7e3e420f94d96
devsaik <emailsaik@gmail.com>
2016-01-12 11:42:14 -0800

    loading repo

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('gulpAngularAbtest')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
