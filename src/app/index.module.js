6f2604ebbe6e2ffb7aa5d4e320073e5df8d29fae
devsaik <emailsaik@gmail.com>
2016-01-12 15:01:58 -0800

    committing all deleted

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('gulpAngularAbtest', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
