773d78590a809e33fc0c87c5c30cc7f496dffd6c
devsaik <emailsaik@gmail.com>
2016-01-13 12:19:09 -0800

    added at.js

---
+++ src/app/optimizely/optimizely.controller.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gulpAngularAbtest')
+    .controller('OptimizelyController', OptimizelyController);
+
+  /** @ngInject */
+  function OptimizelyController($timeout, webDevTec, toastr) {
+    var vm = this;
+
+    vm.awesomeThings = [];
+    vm.classAnimation = '';
+    vm.creationDate = 1452626449447;
+    vm.showToastr = showToastr;
+
+    activate();
+
+    function activate() {
+      getWebDevTec();
+      $timeout(function() {
+        vm.classAnimation = 'rubberBand';
+      }, 4000);
+    }
+
+    function showToastr() {
+      toastr.info('Toastr Invoked');
+      vm.classAnimation = '';
+    }
+
+    function getWebDevTec() {
+      vm.awesomeThings = webDevTec.getTec();
+
+      angular.forEach(vm.awesomeThings, function(awesomeThing) {
+        awesomeThing.rank = Math.random();
+      });
+    }
+  }
+})();
