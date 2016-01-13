81602d4f987dd7fe9b69049555146288ff473790
devsaik <emailsaik@gmail.com>
2016-01-12 18:02:59 -0800

    changes added

--- src/app/components/webDevTec/webDevTec.service.js
+++ src/app/components/webDevTec/webDevTec.service.js
@@ -9,59 +9,18 @@
   function webDevTec() {
     var data = [
       {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+        'title': 'Optimizely',
+        'url': 'https://app.optimizely.com/projects/4565492258/experiments',
+        'description': 'Home page',
+        'logo': 'optimizely.jpeg'
       },
       {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      },
-      {
-        'title': 'Angular UI Bootstrap',
-        'url': 'http://angular-ui.github.io/bootstrap/',
-        'description': 'Bootstrap components written in pure AngularJS by the AngularUI Team.',
-        'logo': 'ui-bootstrap.png'
-      },
-      {
-        'title': 'Stylus',
-        'url': 'http://learnboost.github.io/stylus/',
-        'description': 'Stylus is a revolutionary new language, providing an efficient, dynamic, and expressive way to generate CSS. Supporting both an indented syntax and regular CSS style.',
-        'logo': 'stylus.png'
+        'title': 'Adobe Test and Target',
+        'url': 'https://capitaloneservices.marketing.adobe.com/content/mac/capitaloneservices/target/activities.html',
+        'description': 'Home page',
+        'logo': 'Adobe.png'
       }
+
     ];
 
     this.getTec = getTec;
