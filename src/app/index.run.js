200426bb5fc1fa4e132db33a56a60bc29c0b87ee
devsaik <emailsaik@gmail.com>
2016-01-14 14:41:38 -0800

    manual activation added

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,9 +6,22 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log,$rootScope) {
 
     $log.debug('runBlock end');
+    $rootScope.$on('$stateChangeStart',
+      function(event, toState){
+        try{
+         if(toState.url.indexOf('optimizely')>=0){
+           window.optimizely.push(["activate", 4574084077]);
+         }
+        }
+        catch(ex){
+          $log.error(ex);
+        }
+
+
+      });
   }
 
 })();
